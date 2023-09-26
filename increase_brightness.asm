925ef2d3ff47fd0f47ef627c6b3ace05332e70a2
Nesan Naidoo <ndxnes005@myuct.ac.za>
2023-09-26 21:07:00 +0200

    Made some changes to Q1

--- increase_brightness.asm
+++ increase_brightness.asm
@@ -1,226 +1,107 @@
-.data
-    # Hardcoded PPM header for input and output files
-    inputHeader: .asciiz "P3\n# Hse\n64 64\n255\n"
-    outputHeader: .asciiz "P3\n# Hse\n64 64\n255\n"
-    inputFileName: .asciiz "Desktop/A4/sample_images/LF/house_64_in_ascii_lf.ppm"
-    outputFileName: .asciiz "Desktop/A4/sample_images/LF/house_64_in_ascii_lfV2.ppm"
-    buffer: .space 3      # Buffer size for a single RGB value
-
-    # Variables for holding the sum of RGB values in both images
-    totalR_original: .word 0
-    totalG_original: .word 0
-    totalB_original: .word 0
-    totalR_modified: .word 0
-    totalG_modified: .word 0
-    totalB_modified: .word 0
-
-    # String labels for displaying the averages
-    averageOriginalLabel: .asciiz "Average pixel value of the original image:\n"
-    averageModifiedLabel: .asciiz "Average pixel value of new image:\n"
-    newLine: .asciiz "\n"
+.data 
+filename: .asciiz "/home/shikar/Desktop/mips /jet_64_in_ascii_crlf.ppm"
+filewords: .space 300000
+newline: .asciiz "\n"
+double64: .double 4096.0
 
 .text
 main:
-    # Open the input file for reading
-    li $v0, 13           # syscall code for open
-    la $a0, inputFileName
-    li $a1, 0            # Read mode
-    li $a2, 0            # File permission
+
+    # Open the file for reading
+    li $v0, 13
+    la $a0, filename
+    li $a1, 0
     syscall
-    move $s0, $v0        # Store the file descriptor in $s0
+    move $s0, $v0
 
-    # Open the output file for writing
-    li $v0, 13           # syscall code for open
-    la $a0, outputFileName
-    li $a1, 1            # Write mode
-    li $a2, 0            # File permission
-    syscall
-    move $s1, $v0        # Store the file descriptor in $s1
-
-    # Skip the first four lines (header) in the input file
-    li $t0, 0             # Initialize line counter
-    skip_header:
-        li $v0, 14         # syscall code for read
-        move $a0, $s0      # Input file descriptor
-        la $a1, buffer
-        li $a2, 3          # Buffer size for a single RGB value
-        syscall
-        addi $t0, $t0, 1   # Increment line counter
-        bne $t0, 4, skip_header
-
-    # Write the PPM header to the output file
-    li $v0, 15           # syscall code for write
-    move $a0, $s1        # Output file descriptor
-    la $a1, outputHeader
-    li $a2, 21           # Length of the header
+    # Read the file into memory
+    li $v0, 14
+    move $a0, $s0
+    la $a1, filewords
+    la $a2, 300000
     syscall
 
-    # Loop through each line of pixel data (64x64 lines)
-    li $t4, 64            # Total number of lines (pixels) per color channel
-    li $t5, 0             # Loop counter
+    # Initialize integer value accumulator
+    li $t1, 0
+    li $t2, 10         # ASCII value for newline
+    la $t0, filewords  # Load address of the buffer
+    
 
 loop:
-    # Read a line from the input file (one RGB value)
-    li $v0, 14            # syscall code for read
-    move $a0, $s0         # Input file descriptor
-    la $a1, buffer
-    li $a2, 3             # Buffer size for a single RGB value
-    syscall
+    lb $t3, 23($t0)
+    beqz $t3, end      # Exit loop if we reach the end of the file
 
-    # Convert the ASCII values to integers (assuming valid input)
-    lb $t0, buffer
-    lb $t1, buffer+1
-    lb $t2, buffer+2
+    # Check if it's a valid numeric character (ASCII '0' to '9')
+    li $t4, 48   # ASCII '0'
+    li $t5, 57   # ASCII '9'
+    blt $t3, $t4, not_numeric
+    bgt $t3, $t5, not_numeric
 
-    # Increase each value by 10 (clamp to 255)
-    addi $t0, $t0, 10
+    # Convert ASCII to integer
+    sub $t3, $t3, $t4  # Convert ASCII to integer
+    mul $t1, $t1, 10    # Multiply the current accumulated value by 10
+    add $t1, $t1, $t3   # Add the new digit
+    j continue_loop
+
+not_numeric:
+    # If it's not a valid numeric character, check for newline
+    beq $t3, $t2, print_number
+
+    # If it's not a number or newline, skip it
+    j continue_loop
+
+print_number:
+    # Print the accumulated integer
+average: 
+add $t6, $t6, $t1
+
+add10:
     addi $t1, $t1, 10
-    addi $t2, $t2, 10
-    bgt $t0, 255, clamp_value
-    bgt $t1, 255, clamp_value
-    bgt $t2, 255, clamp_value
-    j write_line
-
-clamp_value:
-    li $t0, 255
-
-write_line:
-    # Write the modified RGB value to the output file
-    sb $t0, buffer
-    sb $t1, buffer+1
-    sb $t2, buffer+2
-    li $v0, 15           # syscall code for write
-    move $a0, $s1        # Output file descriptor
-    la $a1, buffer
-    li $a2, 3            # Buffer size for a single RGB value
+    bgt $t1, 255, change
+ print:
+    add $t8, $t8, $t1
+    li $v0, 1
+    move $a0, $t1
     syscall
 
-    # Update the sum of RGB values for both original and modified images
-    lw $t3, totalR_original
-    lw $t4, totalG_original
-    lw $t5, totalB_original
-    add $t3, $t3, $t0   # $t0 contains the modified R value
-    add $t4, $t4, $t1   # $t1 contains the modified G value
-    add $t5, $t5, $t2   # $t2 contains the modified B value
-    sw $t3, totalR_original
-    sw $t4, totalG_original
-    sw $t5, totalB_original
-
-    lw $t6, totalR_modified
-    lw $t7, totalG_modified
-    lw $t8, totalB_modified
-    add $t6, $t6, $t0   # $t0 contains the modified R value
-    add $t7, $t7, $t1   # $t1 contains the modified G value
-    add $t8, $t8, $t2   # $t2 contains the modified B value
-    sw $t6, totalR_modified
-    sw $t7, totalG_modified
-    sw $t8, totalB_modified
-
-    # Check if we've processed all lines
-    addi $t5, $t5, 1
-    bne $t5, $t4, loop
-
-    # Close input and output files
-    li $v0, 16          # syscall code for close
-    move $a0, $s0       # Input file descriptor
+    # Print a newline
+    li $v0, 4
+    la $a0, newline
     syscall
 
-    li $v0, 16          # syscall code for close
-    move $a0, $s1       # Output file descriptor
-    syscall
+    # Reset accumulator for the next number
+    li $t1, 0
+    j continue_loop
+    
+change:
+addi $t1, $zero, 255
+j print
 
-    # Calculate and display the average RGB values for the original image
-    calculate_average_original:
-        # Calculate average RGB values for the original image
-        lw $t0, totalR_original
-        lw $t1, totalG_original
-        lw $t2, totalB_original
-        li $t3, 4096      # Total number of lines (pixels) per color channel
-        divu $t0, $t0, $t3
-        divu $t1, $t1, $t3
-        divu $t2, $t2, $t3
+continue_loop:
+    addi $t0, $t0, 1   # Move to the next character
+    j loop
 
-        # Convert average RGB values to floating-point double values
-        mtc1 $t0, $f4      # Move average R to $f4
-        mtc1 $t1, $f6      # Move average G to $f6
-        mtc1 $t2, $f8      # Move average B to $f8
-        cvt.d.w $f4, $f4    # Convert R to double
-        cvt.d.w $f6, $f6    # Convert G to double
-        cvt.d.w $f8, $f8    # Convert B to double
+end:
+ 
+       li $t9, 31334400
+       mtc1 $t9, $f14
 
-        # Display label for the average RGB values on the console
-        li $v0, 4            # syscall code for printing a string
-        la $a0, averageOriginalLabel
-        syscall
+mtc1 $t6, $f0
+div.d $f4, $f0, $f14
+li $v0, 3
+add.d $f12, $f4, $f30
+syscall
 
-        # Display the average RGB values on the console
-        li $v0, 3            # syscall code for printing a double
-        mov.d $f12, $f4      # Set $f12 to the value in $f4 (average R)
-        syscall
-        li $v0, 4            # Print a newline
-        la $a0, newLine
-        syscall
+li $v0, 4 
+la $a0, newline
+syscall
 
-        li $v0, 3            # syscall code for printing a double
-        mov.d $f12, $f6      # Set $f12 to the value in $f6 (average G)
-        syscall
-        li $v0, 4            # Print a newline
-        la $a0, newLine
-        syscall
+mtc1 $t8, $f6
+div.d $f10, $f6, $f14
+li $v0, 3
+add.d $f12, $f10, $f30
+syscall
 
-        li $v0, 3            # syscall code for printing a double
-        mov.d $f12, $f8      # Set $f12 to the value in $f8 (average B)
-        syscall
-        li $v0, 4            # Print a newline
-        la $a0, newLine
-        syscall
-
-    # Calculate and display the average RGB values for the new image
-    calculate_average_modified:
-        # Calculate average RGB values for the new image
-        lw $t0, totalR_modified
-        lw $t1, totalG_modified
-        lw $t2, totalB_modified
-        li $t3, 4096      # Total number of lines (pixels) per color channel
-        divu $t0, $t0, $t3
-        divu $t1, $t1, $t3
-        divu $t2, $t2, $t3
-
-        # Convert average RGB values to floating-point double values
-        mtc1 $t0, $f4      # Move average R to $f4
-        mtc1 $t1, $f6      # Move average G to $f6
-        mtc1 $t2, $f8      # Move average B to $f8
-        cvt.d.w $f4, $f4    # Convert R to double
-        cvt.d.w $f6, $f6    # Convert G to double
-        cvt.d.w $f8, $f8    # Convert B to double
-
-        # Display label for the average RGB values on the console
-        li $v0, 4            # syscall code for printing a string
-        la $a0, averageModifiedLabel
-        syscall
-
-        # Display the average RGB values on the console
-        li $v0, 3            # syscall code for printing a double
-        mov.d $f12, $f4      # Set $f12 to the value in $f4 (average R)
-        syscall
-        li $v0, 4            # Print a newline
-        la $a0, newLine
-        syscall
-
-        li $v0, 3            # syscall code for printing a double
-        mov.d $f12, $f6      # Set $f12 to the value in $f6 (average G)
-        syscall
-        li $v0, 4            # Print a newline
-        la $a0, newLine
-        syscall
-
-        li $v0, 3            # syscall code for printing a double
-        mov.d $f12, $f8      # Set $f12 to the value in $f8 (average B)
-        syscall
-        li $v0, 4            # Print a newline
-        la $a0, newLine
-        syscall
-
-    # Exit the program
-    li $v0, 10          # syscall code for exit
+# Exit the program
+    li $v0, 10
     syscall
