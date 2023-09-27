f4194b6a30eff5be4e4804134544531e26931707
Nesan Naidoo <ndxnes005@myuct.ac.za>
2023-09-27 15:29:05 +0200

    made some progress on Q1 and 2

--- greyscale.asm
+++ greyscale.asm
@@ -1,6 +1,8 @@
 .data 
 filename: .asciiz "C:/Users/User/Desktop/A4/A4/house_64_in_ascii_crlf.ppm"
-filewords: .space 300000
+outputfile: .asciiz "C:/Users/User/Desktop/A4/A4/greyscale.ppm"
+header_text:   .asciiz "P2\n# GRY\n64 64\n255\n"
+filewords: .space 100000
 newline: .asciiz "\n"
 double64: .double 4096.0
 
@@ -25,6 +27,27 @@ main:
     li $t1, 0
     li $t2, 10         # ASCII value for newline
     la $t0, filewords  # Load address of the buffer
+
+     # Open the file for writing
+   li $v0, 13       # syscall code for open file
+   la $a0, outputfile # Load the address of the filename
+   li $a1, 1        # Open for write (O_WRONLY)
+   syscall
+   move $s1, $v0    # Store the file descriptor in $s0
+
+   # Write header data to the file
+    li $v0, 15  
+   move $a0, $s1  
+   la $a1, header_text  # Address of the header data
+   la $a2, 19   # Length of the header data
+   syscall
+
+   # Close the file
+  li $v0, 16   
+   move $a0, $s1
+     # syscall code for close file
+   syscall
+
     
 
 loop:
--- increase_brightness.asm
+++ increase_brightness.asm
@@ -1,5 +1,7 @@
 .data 
 filename: .asciiz "C:/Users/User/Desktop/A4/A4/house_64_in_ascii_crlf.ppm"
+outputfile: .asciiz "C:/Users/User/Desktop/A4/A4/house64new.ppm"
+header_text:   .asciiz "P3\n# Hse\n64 64\n255\n"
 filewords: .space 100000
 newline: .asciiz "\n"
 out1: .asciiz "Average pixel value of the original image:\n"
@@ -27,6 +29,28 @@ main:
     li $t1, 0
     li $t2, 10         # ASCII value for newline
     la $t0, filewords  # Load address of the buffer
+
+    # Open the file for writing
+   li $v0, 13       # syscall code for open file
+   la $a0, outputfile # Load the address of the filename
+   li $a1, 1        # Open for write (O_WRONLY)
+   syscall
+   move $s1, $v0    # Store the file descriptor in $s0
+
+   # Write header data to the file
+    li $v0, 15  
+   move $a0, $s1  
+   la $a1, header_text  # Address of the header data
+   la $a2, 19   # Length of the header data
+   syscall
+
+   # Close the file
+  li $v0, 16   
+   move $a0, $s1
+     # syscall code for close file
+   syscall
+
+
     
 
 loop:
